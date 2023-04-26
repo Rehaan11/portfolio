d49d9883b6489598a2ca821fc86e60053b4a693f
Rehaan11 <rameezrehaan@gmail.com>
2023-04-26 10:14:57 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,96 @@
+const logotext = "яℝ";
+const meta = {
+  title: "Rehaan's Portfolio",
+  description: "I’m Rehaan Rameez_ Front-End React Developer",
+};
+
+const introdata = {
+  title: "I’m Rehaan Rameez",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+  },
+  description:
+    "I am an final year undergraduate student based in India who is passionate about Front-End React Developer, and I love to create beautiful and performant products with delightful user experiences.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "Abit About My Self",
+  aboutme:
+    "I’m persuing B.Tech in Electronics and Communication Engineering from RamKrishna Mahato Goverment Engineering College Affilate under MAKAUT I’m intrested in front-end devlopment and currently lerning the backend.",
+};
+
+const skills = [
+  {
+    name: "HTML",
+    value: 80,
+  },
+  {
+    name: "CSS",
+    value: 80,
+  },
+  {
+    name: "Javascript",
+    value: 75,
+  },
+  {
+    name: "React",
+    value: 70,
+  },
+  {
+    name: "SASS",
+    value: 65,
+  },
+  {
+    name: "Java",
+    value: 60,
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://images.unsplash.com/photo-1613575998061-0f59337425f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    description:
+      "Easily track and follow the LIVE prices of over 7,000 cryptocurrencies and access their extensive charts and analytical data. You will be able to select the exchange you're using or look at average prices across all exchanges, also “favorite” coins you want to keep an eye on.",
+    link: "https://crypto-track-trade.vercel.app/",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    description:
+      "UI design services are aimed at creating visually appealing, brand strengthening digital interfaces that attract and retain users.",
+    link: "https://servicestar.vercel.app/",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    description: "Use the Weather app to check the weather condition.",
+    link: "https://js-weather-app-chi.vercel.app/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "rameezrehaan@gmail.com",
+  YOUR_FONE: "+91 9875426360",
+  description: "👆 You can contact via my email or phone No.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Rehaan11",
+  facebook: "https://www.facebook.com/rehaan.ramirez.7/",
+  linkedin: "https://www.linkedin.com/in/rehaan-rameez-8730851b1/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  skills,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
